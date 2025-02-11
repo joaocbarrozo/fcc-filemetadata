431fa0afff6bff7fa4e1baa6fac53ff3072a77e9
joaocbarrozo <97959049+joaocbarrozo@users.noreply.github.com>
2025-02-11 14:46:46 -0300

    Update index.js all tests passed

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer')
 require('dotenv').config()
 
 var app = express();
+
+// multer for file upload
+var upload = multer({ dest: 'uploads/' });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +15,23 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// File upload route
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  // Check if a file was uploaded
+  if (!req.file) {
+    return res.json({ 'error': 'No file uploaded' });
+  }
 
+  // Get file details
+  const fileDetails = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  };
+
+  // Send the file details as a JSON response
+  res.json(fileDetails);
+});
 
 
 const port = process.env.PORT || 3000;
